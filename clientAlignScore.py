f9ea144a7613be5c6be1d5162069946f15262ef2
durgaravi <v2durga@gmail.com>
2016-11-08 21:06:12 -0800

    adding cosine similarity code

--- clientAlignScore.py
+++ clientAlignScore.py
@@ -3,6 +3,9 @@ sys.path.append('monolingual-word-aligner')
 from glob import glob
 
 from aligner import *
+from collections import Counter
+import re
+import math
 
 def read_training_file(filename):
 	with open(filename) as f:
@@ -22,13 +25,32 @@ def get_alignment_score(sentence_pair):
 	prop2 = align_count2/float(content_count2)
 	return (2*prop1*prop2)/(prop1 + prop2)
 
+def get_cosine_similarity(sentence_pair):
+	sentence1 = Counter(word_token_expr.findall(sentence_pair[0]))
+	sentence2 = Counter(word_token_expr.findall(sentence_pair[1]))
+	common_words = set(sentence1.keys()) & set(sentence2.keys())
+    	common_count = sum([sentence1[x] * sentence2[x] for x in common_words])
+    	sum1 = sum([sentence1[x]**2 for x in sentence1.keys()])
+    	sum2 = sum([sentence2[x]**2 for x in sentence2.keys()])
+	union_count = math.sqrt(sum1) * math.sqrt(sum2)
+    
+    	if not union_count:
+        	return 0
+	else:
+		return float(common_count)/union_count
+
 def read_training_data(category):
 	files = [f for f in glob("data/*") if "train" in f]
 	training_sentence_files = [f for train_dir in files for f in glob(train_dir+"/data/*")  if category in f]
 	training_gs_files = [f for train_dir in files for f in glob(train_dir+"/gs/*") if category in f]
 	training_sentences = [x for f in training_sentence_files for x in read_training_file(f)]
 	gold_standard_scores = [x for f in training_gs_files for x in read_training_gs(f)]
+	return training_sentences, gold_standard_scores
 	
 if __name__ == "__main__": 
 	training_sentences, gold_standard_scores = read_training_data("c4")
-	predicted_alignment_scores = map(get_alignment_score, training_sentences)
+	predicted_alignment_scores = map(get_alignment_score, training_sentences[:2])
+
+	word_token_expr = re.compile(r'\w+')
+	predicted_cosine_similarity_scores = map(get_cosine_similarity, training_sentences[:2])
+	print predicted_alignment_scores, predicted_cosine_similarity_scores
