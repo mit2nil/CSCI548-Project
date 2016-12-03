58e21f4265b73189259e738de99ba0b4af742896
durgaravi <v2durga@gmail.com>
2016-12-03 15:42:47 -0800

    try-except modification

--- clientAlignScore.py
+++ clientAlignScore.py
@@ -6,6 +6,7 @@ from aligner import *
 from collections import Counter
 import re
 import math
+from scipy.stats import pearsonr
 
 def read_training_file(filename):
 	with open(filename) as f:
@@ -16,14 +17,21 @@ def read_training_gs(filename):
 		return [float(x) for x in f.readlines()]
 
 def get_alignment_score(sentence_pair):
-	alignment = align(sentence_pair[0].decode("utf-8").encode("ascii","ignore"), sentence_pair[1].decode("utf-8").encode("ascii","ignore"))
+	try:
+		alignment = align(sentence_pair[0].decode("utf-8").encode("ascii","ignore"), sentence_pair[1].decode("utf-8").encode("ascii","ignore"))
+	except:
+		alignment = align(re.sub("[#(){}:]+","",sentence_pair[0]).decode("utf-8").encode("ascii","ignore"), re.sub("[#(){}:]+","",sentence_pair[1]).decode("utf-8").encode("ascii","ignore"))
 	align_count1 = len([x[0] for x in alignment[1] if x[0] not in stopwords + punctuations + ['\'s', '\'d', '\'ll']])
 	align_count2 = len([x[1] for x in alignment[1] if x[1] not in stopwords + punctuations + ['\'s', '\'d', '\'ll']])
 	content_count1 = len([x for x in alignment[2] if x not in stopwords + punctuations + ['\'s', '\'d', '\'ll']])
 	content_count2 = len([x for x in alignment[3] if x not in stopwords + punctuations + ['\'s', '\'d', '\'ll']])
 	prop1 = align_count1/float(content_count1)
 	prop2 = align_count2/float(content_count2)
-	return (2*prop1*prop2)/(prop1 + prop2)
+	if prop1 and prop2:
+		return (2*prop1*prop2)/(prop1 + prop2)
+	else:
+		return 0
+	
 
 def get_cosine_similarity(sentence_pair):
 	sentence1 = Counter(word_token_expr.findall(sentence_pair[0]))
@@ -48,9 +56,13 @@ def read_training_data(category):
 	return training_sentences, gold_standard_scores
 	
 if __name__ == "__main__": 
-	training_sentences, gold_standard_scores = read_training_data("c4")
-	predicted_alignment_scores = map(get_alignment_score, training_sentences[:2])
-
+	training_sentences, gold_standard_scores = read_training_data("c5")
+	predicted_alignment_scores = map(get_alignment_score, training_sentences)
+	print len(training_sentences)
+	print pearsonr(gold_standard_scores, predicted_alignment_scores)[0]
+	
 	word_token_expr = re.compile(r'\w+')
-	predicted_cosine_similarity_scores = map(get_cosine_similarity, training_sentences[:2])
-	print predicted_alignment_scores, predicted_cosine_similarity_scores
+	predicted_cosine_similarity_scores = map(get_cosine_similarity, training_sentences)
+	#print predicted_alignment_scores, predicted_cosine_similarity_scores
+
+	print pearsonr(gold_standard_scores, predicted_cosine_similarity_scores)[0]
