d3420d09bffcf0a8629d483ae9191321e415e368
Nilay Chheda <mit2nil@gmail.com>
2016-12-03 20:45:43 -0800

    Added umbc score and polished code flow and output

--- README.md
+++ README.md
@@ -13,16 +13,16 @@
 
 * git clone git://github.com/dasmith/stanford-corenlp-python.git
 * cd stanford-corenlp-python
-* replace below line in the stanford-corenlp-python/corenlp.py with line below it.
-* ''' rel, left, right = map(lambda x: remove_id(x), split_entry) '''
-* ''' rel, left, right = split_entry'''
+* Replace below line in the stanford-corenlp-python/corenlp.py with line below it.
+* ``` rel, left, right = map(lambda x: remove_id(x), split_entry) ``` with 
+* ``` rel, left, right = split_entry ```
 * wget http://nlp.stanford.edu/software/stanford-corenlp-full-2014-08-27.zip
 * unzip stanford-corenlp-full-2014-08-27.zip
-* python corenlp.py
+* Run server by ```python corenlp.py```
 * open another terminal in the same directory 
 * cd <path>/CSCI548-Project
 * python -m nltk.downloader stopwords
 * add below lines in the monolingual-word-aligner/wordSim.py inside function loadPPDB
-''' import os '''
-''' ppdbFileName = os.path.join(os.path.dirname(os.path.realpath(__file__)),ppdbFileName) '''
-* python clientAlignScore.py
+``` import os 
+	ppdbFileName = os.path.join(os.path.dirname(os.path.realpath(__file__)),ppdbFileName) ```
+* Run ```python clientAlignScore.py```
--- semeval.py
+++ semeval.py
@@ -10,26 +10,24 @@ import math
 from scipy.stats import pearsonr
 from sklearn.linear_model import Ridge
 
-def read_training_file(filename):
+def read_sentence_file(filename):
 	with open(filename) as f:
 		return [x.strip().split("\t") for x in f.readlines()]
 
-def read_training_gs(filename):
+def read_gs_file(filename):
 	with open(filename) as f:
 		return [float(x) for x in f.readlines()]
 
-def get_umbc_score(sentence_pair, type='relation', corpus='webbase'):
-    sss_url = "http://swoogle.umbc.edu/SimService/GetSimilarity"
-    try:
-        response = get(sss_url, params={'operation':'api','phrase1':sentence_pair[0],'phrase2':sentence_pair[1],'type':type,'corpus':corpus})
-        return float(response.text.strip())
-    except:
-        print 'Error in getting similarity for %s: %s' % ((s1,s2), response)
-        return 0.0
+def get_umbc_score(sentence_pair):
+	endpoint = "http://swoogle.umbc.edu/StsService/GetStsSim"
+	try:
+		response = get(endpoint, params={'operation':'api','phrase1':sentence_pair[0],'phrase2':sentence_pair[1]})
+		return float(response.text.strip())
+	except:
+		print 'Error in getting similarity for %s: %s' % ((sentence_pair[0],sentence_pair[1]), response)
+		return 0.0
 
 def get_alignment_score(sentence_pair):
-
-	#print ("Inside get_alignment_score")
 	try:
 		alignment = align(sentence_pair[0].decode("utf-8").encode("ascii","ignore"), sentence_pair[1].decode("utf-8").encode("ascii","ignore"))
 	except:
@@ -47,7 +45,6 @@ def get_alignment_score(sentence_pair):
 		return 0
 
 def get_cosine_similarity(sentence_pair):
-	#print ("Inside get_cosine_similarity")
 	sentence1 = Counter(word_token_expr.findall(sentence_pair[0]))
 	sentence2 = Counter(word_token_expr.findall(sentence_pair[1]))
 	common_words = set(sentence1.keys()) & set(sentence2.keys())
@@ -61,33 +58,52 @@ def get_cosine_similarity(sentence_pair):
 	else:
 		return float(common_count)/union_count
 
-def read_training_data(category,dataset_type):
-	#print ("Inside read_training_data")
+def read_data(category, dataset_type):
 	files = [f for f in glob("data/*") if dataset_type in f]
-	training_sentence_files = [f for train_dir in files for f in glob(train_dir+"/data/*")  if category in f]
-	training_gs_files = [f for train_dir in files for f in glob(train_dir+"/gs/*") if category in f]
-	training_sentences = [x for f in training_sentence_files for x in read_training_file(f)]
-	gold_standard_scores = [x for f in training_gs_files for x in read_training_gs(f)]
-	#print ("Parsed all training and gold standard data")
-	return training_sentences, gold_standard_scores
+	sentence_files = [f for train_dir in files for f in glob(train_dir+"/data/*")  if category in f]
+	gs_files = [f for train_dir in files for f in glob(train_dir+"/gs/*") if category in f]
+	sentences = [x for f in sentence_files for x in read_sentence_file(f)]
+	gold_standard_scores = [x for f in gs_files for x in read_gs_file(f)]
+	return sentences, gold_standard_scores
 	
 if __name__ == "__main__": 
 	for category in ["c1","c2","c3","c4","c5"]:
-		print "Score for "+category
-		training_sentences, gold_standard_scores = read_training_data(category, "test")
-		predicted_alignment_scores = map(get_alignment_score, training_sentences)
-		print pearsonr(gold_standard_scores, predicted_alignment_scores)[0]
-		word_token_expr = re.compile(r'\w+')
-		predicted_cosine_similarity_scores = map(get_cosine_similarity, training_sentences)
-		print pearsonr(gold_standard_scores, predicted_cosine_similarity_scores)[0]
 		
-		ridge_training_sentences, ridge_gold_standard_scores = read_training_data(category, "train")
+		print "\n# Running Semantic Textual Similarity for SemEval category : "+category+"\n"
+		test_sentences, gold_standard_scores = read_data(category,"test")
+		
+		print "# Method 1"
+		print "# SemEval 2015 rank 5 - DLS@CU-U"
+		predicted_alignment_scores = map(get_alignment_score, test_sentences)
+		print "Pearson coefficient : "+pearsonr(gold_standard_scores, predicted_alignment_scores)[0]
+		print "\n"
+
+		print "# Method 2"
+		print "# SemEval 2013 rank 1 - UMBC EBIQUITY-CORE"
+		predicted_umbc_scores = map(get_umbc_score, test_sentences)
+		print "Pearson coefficient : "+pearsonr(gold_standard_scores, predicted_umbc_scores)[0]
+		print "\n"
+
+		print "# Method 3"
+		print "# SemEval 2015 rank 1+3 - DLS@CU-S1 and DLS@CU-S2"
+		word_token_expr = re.compile(r'\w+')
+		predicted_cosine_similarity_scores = map(get_cosine_similarity, test_sentences)
+
+		# Get the training data
+		ridge_training_sentences, ridge_gold_standard_scores = read_data(category, "train")
+
+		# Feature 1 - DLS@CU-U  
 		ridge_predicted_alignment_scores = map(get_alignment_score, ridge_training_sentences)
+		# Feature 2 - Cosine similarity
 		ridge_predicted_cosine_similarity_scores = map(get_cosine_similarity, ridge_training_sentences)
+		
+		# Ridge training
 		X_train = zip(ridge_predicted_alignment_scores,ridge_predicted_cosine_similarity_scores)
 		ridge_classifier = Ridge(alpha = 1.0)
 		ridge_classifier.fit(X_train,ridge_gold_standard_scores)
 
+		# Ridge Prediction
 		X_test = zip(predicted_alignment_scores, predicted_cosine_similarity_scores)
-		predicted_scores = ridge_classifier.predict(X_test)
-		print pearsonr(gold_standard_scores, predicted_scores)[0]
+		predicted_ridge_scores = ridge_classifier.predict(X_test)
+		print "Pearson coefficient : "+pearsonr(gold_standard_scores, predicted_ridge_scores)[0]
+		print "\n"
